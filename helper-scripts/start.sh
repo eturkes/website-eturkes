36a617e2aca0c23bd38537b09d7a06b8e36fcb2d
Emir Turkes <eturkes@bu.edu>
2018-10-23 04:51:25 -0700

    Fix path to other scripts

--- helper-scripts/start.sh
+++ helper-scripts/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Start the web server after building
 
-./scripts/nginx-proxy.sh \
-    && ./scripts/letsencrypt-nginx-proxy-companion.sh \
+./helper-scripts/nginx-proxy.sh \
+    && ./helper-scripts/letsencrypt-nginx-proxy-companion.sh \
     && docker-compose up
