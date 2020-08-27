fd47554f074ced522720b18dbea8c8cf8efc4a4b
Emir Turkes <eturkes@bu.edu>
2020-08-27 11:59:57 -0400

    Update container names for new versions of Docker

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,7 +11,7 @@ google-drive-ocamlfuse -label eturkes@bu.edu ~/gdrive \
     && docker-compose build \
     && docker-compose up -d db \
     && divio project import db gdrive/database.dump \
-    && cat gdrive/database.dump | docker exec -i websiteeturkes_db_1 psql -U postgres db \
+    && cat gdrive/database.dump | docker exec -i website-eturkes_db_1 psql -U postgres db \
     && docker-compose run web start migrate \
     && docker-compose run --rm web gulp build \
     && docker-compose run --rm web python manage.py collectstatic \
--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -8,7 +8,7 @@ docker-compose pull \
     && docker-compose build \
     && docker-compose up -d db \
     && divio project import db gdrive/database.dump \
-    && cat gdrive/database.dump | docker exec -i websiteeturkes_db_1 psql -U postgres db \
+    && cat gdrive/database.dump | docker exec -i website-eturkes_db_1 psql -U postgres db \
     && docker-compose run web start migrate \
     && docker-compose run --rm web gulp build \
     && docker-compose run --rm web python manage.py collectstatic
