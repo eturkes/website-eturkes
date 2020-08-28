5dfbdeeee8818004159171543edf844b806f6862
Emir Turkes <eturkes@bu.edu>
2020-08-28 10:57:23 -0400

    Update additional container name

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -6,7 +6,7 @@
 
 google-drive-ocamlfuse -label eturkes@bu.edu ~/gdrive \
     && mv gdrive/database.dump gdrive/media.tar.gz gdrive/old \
-    && docker exec websiteeturkes_db_1 pg_dump -U postgres db > gdrive/database.dump \
+    && docker exec website-eturkes_db_1 pg_dump -U postgres db > gdrive/database.dump \
     && cd data \
     && sudo tar -zcf ../gdrive/media.tar.gz ./ \
     && cd .. \
